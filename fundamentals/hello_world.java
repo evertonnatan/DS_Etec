d76295d77d7efbd56d89c9548672ea07be5d65bb
evertonnatan <evertonnatandl@outlook.com>
2022-08-26 20:54:40 -0300

    Iniciando os estudos de Java

---
+++ fundamentals/hello_world.java
@@ -0,0 +1,11 @@
+//package hello_world;
+
+public class hello_world {
+public static void main(String[] args) {
+    
+    String meuPrimeiroPrograma;
+    meuPrimeiroPrograma = "Hello, World!";
+
+    System.out.println(meuPrimeiroPrograma);
+}
+}
