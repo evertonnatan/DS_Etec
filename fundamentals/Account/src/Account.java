d7ff6a67234212d95c36ac6bfc2170a54eead73c
Everton Natã <37844708+evertonnatan@users.noreply.github.com>
2022-10-22 14:28:45 -0300

    Iniciando os estudos de POO

---
+++ fundamentals/Account/src/Account.java
@@ -0,0 +1,11 @@
+public class Account {
+    private String name; // private é um modificador de acesso, da mesma forma que public também é
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+         return name;
+    }
+}
